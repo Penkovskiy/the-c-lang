e60f5b874969ff5895f70b27d59a98483e84bf5c
Eugene Penkovskiy <e.penkovskiy@gmail.com>
2018-02-22 13:12:14 +0200

    Fix calculation with floating point

--- far2celc.c
+++ far2celc.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-	int fahr;
-	int celcius;
+	float fahr;
+	float celcius;
 
 	int lower, upper, step;
 
@@ -14,8 +14,8 @@ int main()
 
 	fahr = lower;
 	while (fahr <= upper) {
-		celcius = 5 * (fahr - 32) / 9;
-		printf("%3d\t%6d\n", fahr, celcius);
+		celcius = (5.0 / 9.0) * (fahr - 32.0);
+		printf("%3.0f\t%6.1f\n", fahr, celcius);
 		fahr = fahr + step;
 	}
 	
