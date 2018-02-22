71ce302adb8a23679cd1cb8727bee469a5b27ecd
Eugene Penkovskiy <e.penkovskiy@gmail.com>
2018-02-22 15:56:46 +0200

    Make hometask

--- iocopy.c
+++ iocopy.c
@@ -1,15 +1,35 @@
 // iocopy.c - Копирует входной поток символов в выходной поток
 #include <stdio.h>
 
+#define MAX_COUNT 10
+
 int main()
 {
     int c; // Переменная с имеет тип int
            // Чтобы иметь возможность Содержать значение
            // EOF
     
+    int count;
+    int condition;
 
     while ((c = getchar()) != EOF)  // End Of File
-        puthcar(c);
+        putchar(c);
     
+    // Упражнение 1.6
+    // Убедитесь что выражение getchar() != EOF получает значение
+    // 0 или 1
+    for (count = 0; count < MAX_COUNT; count++) {
+        condition = getchar() != EOF;
+        if ((condition != 1) || (condition != 0))
+            printf("Ok\n");
+        else
+            printf("Failed!: %d\n", condition);
+    }
+        
+    
+    // Упражнение 1.7
+    // Напечатайте значение EOF
+    printf("EOF = %d\n", EOF);
+
     return 0;
 }
