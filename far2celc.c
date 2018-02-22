1dd3edc2a2701dc16b32bf4f5a48e4a5fa1786d3
Eugene Penkovskiy <e.penkovskiy@gmail.com>
2018-02-22 12:23:40 +0200

    Add far2celc.c - show table Celc to Farh

---
+++ far2celc.c
@@ -0,0 +1,23 @@
+// far2cel.c - Печатает таблицы температур Целсий - Фаренгейт
+#include <stdio.h>
+
+int main()
+{
+	int fahr;
+	int celcius;
+
+	int lower, upper, step;
+
+	lower = 0; // Нижняя граница таблицы температур
+	upper = 300; // Верхняя граница таблицы температур
+	step = 20; // Шаг изменения температуры Цельсия
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celcius = 5 * (fahr - 32) / 9;
+		printf("%d\t%d\n", fahr, celcius);
+		fahr = fahr + step;
+	}
+	
+	return 0;
+}
