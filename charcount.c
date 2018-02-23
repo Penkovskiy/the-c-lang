26a6b40b05bf753a014c7053dbb097904219c0fe
Eugene Penkovskiy <e.penkovskiy@gmail.com>
2018-02-23 22:26:11 +0200

    charcount.c refactor loop (version 2)

--- charcount.c
+++ charcount.c
@@ -1,3 +1,12 @@
 // charcount.c - Подсчет символов stdin
 #include <stdio.h>
 
+int main()
+{
+    double nc;
+    for (nc = 0; getchar() != EOF; nc++)
+        ;
+    printf("%.0f\n", nc);
+
+    return 0;
+}
