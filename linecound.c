150c6f40e5149d911597dfd141e7c8cca68f1f04
Eugene Penkovskiy <e.penkovskiy@gmail.com>
2018-02-23 22:27:01 +0200

    Add linecount - count number of line in stdin

---
+++ linecound.c
@@ -0,0 +1,14 @@
+// linecount.c - Считает кол-во строк, вводимых в stdin
+#include <stdio.h>
+
+int main()
+{
+    int c, nl;
+    nl = 0;
+    while ((c = getchar()) != EOF)
+        if (c == '\n')
+            ++nl;
+    printf("%d\n", nl);
+
+    return 0;
+}
