aecd07b092fd729060041bb42ba6012360886074
Eugene Penkovskiy <e.penkovskiy@gmail.com>
2018-02-24 13:19:40 +0200

    Add newline_worlds.c Exercise 1.12

---
+++ newline_worlds.c
@@ -0,0 +1,15 @@
+// newline_worlds.c - Печатает каждое слово с новой строки
+#include <stdio.h>
+
+int main()
+{
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\t' || c == '\n')
+            putchar('\n')
+        else 
+            putchar(c);
+    }
+    return 0;
+}
