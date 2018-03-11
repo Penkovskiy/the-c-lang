f51481b0b97e905fdd4c154aca205cfdeb9abbc4
Eugene Penkovskiy <e.penkovskiy@gmail.com>
2018-03-12 00:52:08 +0200

    Add basic strcat funtion

--- 2types/basic.c
+++ 2types/basic.c
@@ -6,9 +6,33 @@ void squeeze(char s[], int c)
     int i;
     int j;
 
-    for (i =  = 0; s[i] != '\0'; i++)
+    for (i = j = 0; s[i] != '\0'; i++)
         if (s[i] != c)
             s[j++] = s[i];
     s[j] = '\0';
 }
+
+// strcat: помещает строку t в конец s
+// Размер не проверяется
+void strcat(char s[], char t[])
+{
+    int i;
+    int j;
+
+    i = j = 0;
+    while (s[i] != '\0')
+        i++;
+    while ((s[i++] = t[j++]) != '\0')
+        ;
+}
+
+int main()
+{
+    char str[100] = "Hello, ";
+    char end[] = "World";
+
+    strcat(str, end);
+    printf("%s\n", str);
+}
+
 
